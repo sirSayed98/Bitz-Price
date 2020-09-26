893648202ebc227e68a6852ea56f3819b9e3dcec
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-09-26 20:59:51 +0200

    add navBar from bootswatch

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,21 @@
 import Link from 'next/link'
 const Navbar = () => (
     <>
-        <ul>
-            <li><Link href='/'><a> home</a></Link> </li>
-            <li><Link href='/about'><a> about</a></Link> </li>
-        </ul>
+        <nav className="navbar navbar-expand navbar-dark bg-dark mb-4">
+            <div className="container">
+               <Link href="/"><a className="navbar-brand">BitzPrice</a></Link>
+                <div className="collapse navbar-collapse">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item">
+                            <Link href="/"><a className="nav-link">Home</a></Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link href="/about"><a className="nav-link">About</a></Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </>
 );
 
