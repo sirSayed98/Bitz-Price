f572ffd19f385c0716fe003ce49ed17bb2191f33
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-09-26 20:32:07 +0200

    add Link and navigation between two pages

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,12 @@
 import Link from 'next/link'
-const index = () => (
+const about = () => (
     <>
-        <div><h1> hello from index</h1> </div>
+        <div><h1> hello from about</h1> </div>
         <ul>
             <li><Link href='/'><a> home</a></Link> </li>
             <li><Link href='/about'><a> about</a></Link> </li>
         </ul>
     </>
+
 );
-
-
-
-export default index
+export default about
