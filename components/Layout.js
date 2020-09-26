e0422f50d5c096f15a3fdaa980ded61a92f23c2e
mohamed sayed <mohamed.sayed8899@gmail.com>
2020-09-26 20:55:41 +0200

    add Head for pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,11 @@
 import Navbar from './Navbar'
-
+import Head from 'next/head';
 const Layout = (props) => (
     <>
+        <Head>
+            <title>BitzPrice</title>
+            <link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css" />
+        </Head>
         <Navbar />
         {props.children}
     </>
